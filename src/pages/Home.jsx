bffe217c3eb77348475bd0ccd4355c0fec377973
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 22:48:46 +0530

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>
+        Welcome to Home page
+      </h1>
+      <div className="img">
+        <img
+
+        src="https://cdn.pixabay.com/photo/2024/03/04/14/56/pagoda-8612554_1280.jpg"
+        alt="London Bridge"
+        
+        />
+      </div>
+    </div>
   )
 }
 
