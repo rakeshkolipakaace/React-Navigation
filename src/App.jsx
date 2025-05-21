d11dccf92ae08edd66a81ee679134bda0ba25a95
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 19:42:58 +0530

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router> 
   )
 }
 
