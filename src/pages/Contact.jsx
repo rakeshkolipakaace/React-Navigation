e5ab4295758710976172ba3dcc0e4190362c5243
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 22:48:24 +0530

    contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+       <h1>
+        Welcome to Contact page
+      </h1>
+      <div className="img">
+        <img
+
+        src="https://cdn.pixabay.com/photo/2016/11/14/03/21/fireworks-1822479_1280.jpg"
+        alt="London Bridge"
+        
+        />
+      </div>
+    </div>
   )
 }
 
