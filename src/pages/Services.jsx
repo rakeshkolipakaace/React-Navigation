a4b7688f2e258bf46b00fed936907c97298530a2
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 22:49:10 +0530

    Sevices.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+       <h1>
+        Welcome to Services page
+      </h1>
+      <div className="img">
+        <img
+
+        src="https://cdn.pixabay.com/photo/2024/07/11/18/48/angel-8888879_1280.jpg"
+        alt="London Bridge"
+        
+        />
+      </div>
+    </div>
   )
 }
 
