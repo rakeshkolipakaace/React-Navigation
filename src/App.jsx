4141f7bb6a670735805634aae58eb60562dd84d4
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 19:53:34 +0530

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router> 
   )
