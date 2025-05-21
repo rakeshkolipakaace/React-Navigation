a69fb6e9a247edb2218382a8b2d3d59b2992bdb8
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 20:00:02 +0530

    Fabars and Fatimes import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,29 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { FaBars , FaTimes} from "react-icons/fa";
+
+
 const Navbar = () => {
   return (
     <>
 
     <div className="Navbar">
-      <div className="navbar-logo"></div>
+      <div className="navbar-logo">React Navigation</div>
 
-      <ul>
+      <ul  className="navbar-links"> 
+        <button  className="closed-drawer">
+            <FaTimes />
+        </button>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
+
+      <div className="menu-icon">
+        <FaBars/>
+      </div>
     </div>
     </>
    
