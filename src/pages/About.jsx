6829f4df5514f53bfdb94cf922c4084a143cd74c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-21 22:48:02 +0530

    About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+       <h1>
+        Welcome to About page
+      </h1>
+      <div className="img">
+        <img
+
+        src="https://cdn.pixabay.com/photo/2021/11/12/20/57/church-6790038_1280.jpg"
+        alt="London Bridge"
+        
+        />
+      </div>
+    </div>
   )
 }
 
